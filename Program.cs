0927632311e2b9f2d56f2fc0eac651e5c639c5f7
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2020-02-27 23:28:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.EJ2.Blazor;
+
+namespace My_WebAssembly_App
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here.");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
+            await builder.Build().RunAsync();
+        }
+    }
+}
