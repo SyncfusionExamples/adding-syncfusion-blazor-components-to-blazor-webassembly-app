f668bcf0cdd86b62c6c80eb38c04af32b0cfa39b
UmaMaheswariChandrabose <umamaheswaric@syncfusion.com>
2020-03-25 17:29:18 +0530

    Changes done

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Syncfusion.EJ2.Blazor;
+using Syncfusion.Blazor;
 
-namespace My_WebAssembly_App
+namespace my_blazor_app
 {
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here.");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Use your License Key here.");
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
             builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
